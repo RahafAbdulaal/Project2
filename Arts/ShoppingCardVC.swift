b8653127819ddf156fdc857a8ec8eea113260488
RahafAbdulaal <Rahaf.Abdulaal.med.s@tuwaiq.edu.sa>
2021-11-04 02:27:12 +0300

    The README

---
+++ Arts/ShoppingCardVC.swift
@@ -0,0 +1,30 @@
+//
+//  ShoppingCardVC.swift
+//  Arts
+//
+//  Created by Rahaf Abdulaal on 28/03/1443 AH.
+//
+
+import UIKit
+
+class ShoppingCardVC: UIViewController {
+
+    @IBOutlet weak var CardTable: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Project2
+# Project2-store for artists
+
+## The goal of the application is to provide products for drawing, ease of browsing and purchasing processes for users
+
+## Users stories 
+- the user can Browse 
+- the user can add Credit card
+- the user can delete Credit card
+- the user can update Credit card information 
+
+## problems and Future work
+- it was very hard to pass the data to the cart shopping
+- i will add quantity function for items and passing the data 
+
+## favorite function 
+- maybe the delete function beacuse it's very easy and helpful
+
+
